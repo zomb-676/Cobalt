5c52e80d92a36e3b0fcdd3ba302a0588c52629a1
zomb_676 <zomb_676@qq.com>
2022-02-07 05:11:05 +0800

    switch to core profile

--- cobalt-graphic/src/main/kotlin/zomb_676/cobalt/grahic/Main.kt
+++ cobalt-graphic/src/main/kotlin/zomb_676/cobalt/grahic/Main.kt
@@ -3,18 +3,25 @@ package zomb_676.cobalt.grahic
 import org.lwjgl.glfw.GLFW
 import org.lwjgl.opengl.GL
 import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL43
+import org.lwjgl.opengl.GL46
 import org.lwjgl.system.MemoryUtil
 import kotlin.concurrent.thread
 
 fun main() {
     init()
+    GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR,4)
+    GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR,6)
+    GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE,GLFW.GLFW_OPENGL_FORWARD_COMPAT)
+    GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT,GLFW.GLFW_FALSE)
+
     val window = createWindow(900, 900, "rua")
     GLFW.glfwMakeContextCurrent(window)
+    GLFW.glfwMakeContextCurrent(window)
     GL.createCapabilities()
-    val id = GLFW.glfwGetCurrentContext()
-    GLFW.glfwMakeContextCurrent(id)
 
     while (!GLFW.glfwWindowShouldClose(window)) {
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT)
 
         GL11.glBegin(GL11.GL_TRIANGLES)
         GL11.glColor3f(1f, 0f, 0f)
@@ -24,11 +31,11 @@ fun main() {
         GL11.glColor3f(0f, 0f, 1f)
         GL11.glVertex2f(0.5f, -0.5f)
         GL11.glEnd()
-        thread {  }.start()
 
         GLFW.glfwSwapBuffers(window)
         GLFW.glfwPollEvents()
     }
+
     GLFW.glfwDestroyWindow(window)
     GLFW.glfwTerminate()
     GLFW.glfwSetErrorCallback(null)?.free()
